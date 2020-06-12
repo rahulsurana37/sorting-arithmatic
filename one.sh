c0a2ef3f2806c63d05da884fb7dcfca76be3de65
unknown <rahulsurana37@gmail.com>
2020-06-12 16:56:51 +0530

    Added results to dictionary

--- one.sh
+++ one.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+declare -A arithmetic
+
 echo "Welcome. This is the arithmetic code."
 read -p "Enter the first input: " a
 read -p "Enter the second value: " b
 read -p "Enter the third value: " c
 
 result1=$((($a+$b)*$c))
-echo $result1
+arithmetic[one]=$result1
+#echo $result1
 
 result2=$((($a*$b)+$c))
-echo $result2
+arithmetic[two]=$result2
+#echo $result2
 
 result3=$((($c+$a)/$b))
-echo $result3
+arithmetic[three]=$result3
+#echo $result3
 
-result4=$((($a%$b)+$C))
-echo $result4
+result4=$((($a%$b)+$c))
+arithmetic[four]=$result4
+#echo $result4
+
+echo ${arithmetic[@]}
